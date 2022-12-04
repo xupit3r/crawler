9d30f32d2dc3574ff0f674e7e1a26c22eceba474
Joe D'Alessandro <joe@thejoeshow.net>
2022-12-04 10:38:28 -0500

    updated crawler to actually execute multiple async requests at a time

--- src/lib/crawl.ts
+++ src/lib/crawl.ts
@@ -7,6 +7,10 @@ import { isCoolDownStatus } from './utils';
 
 const logger = debug('crawler');
 
+const requests: Array<() => Promise<Boolean>> = [];
+
+const MAX_REQUESTS = 5;
+
 /**
  * Simple crawler. Give it a URL, it does its thing.
  * 
@@ -28,35 +32,49 @@ export const crawl = async (options: CrawlerOptions) => {
     logger('reading from queue');
   }
 
-  forever(async next => {
-    const nextVisit = await getNextLink(options.limitTo);
+  forever(next => {
 
-    if (nextVisit !== null) {
-      const link = nextVisit.url;
-      
-      logger(`processing ${link}`);
+    if (requests.length < MAX_REQUESTS) {
+      const currentIdx = requests.length - 1;
 
-      try {
-        const page = await processPage(link);
-        logger(`retrieved page ${page.url}`);
-      } catch (err) {
-        let crawlerError = err as CrawlerError;
+      requests.push(async () :Promise<Boolean> => {
+        const nextVisit = await getNextLink(options.limitTo);
 
-        logger(`failed to retrieve ${link} -- ${crawlerError.message}`);
-
-        if (isCoolDownStatus(crawlerError.status)) {
-          const waitTime: number = (typeof crawlerError.headers['Retry-After'] !== 'undefined' 
-            ? Number.parseInt(crawlerError.headers['Retry-After']) 
-            : 3600
-          );
-
-          await addHostToCooldown(crawlerError.host, waitTime);
+        if (nextVisit !== null) {
+          const link = nextVisit.url;
+          
+          logger(`processing ${link}`);
+    
+          try {
+            const page = await processPage(link);
+            logger(`retrieved page ${page.url}`);
+          } catch (err) {
+            let crawlerError = err as CrawlerError;
+    
+            logger(`failed to retrieve ${link} -- ${crawlerError.message}`);
+    
+            if (isCoolDownStatus(crawlerError.status)) {
+              const waitTime: number = (typeof crawlerError.headers['Retry-After'] !== 'undefined' 
+                ? Number.parseInt(crawlerError.headers['Retry-After']) 
+                : 3600
+              );
+    
+              await addHostToCooldown(crawlerError.host, waitTime);
+            }
+          } finally {
+            await removeFromQueue(nextVisit.url);
+          }
         }
-      } finally {
-        await removeFromQueue(nextVisit.url);
-      }
 
-      return setTimeout(next, 250);
+
+        return true;
+      });
+
+      requests[requests.length - 1]().then(() => {
+        requests.splice(currentIdx, 1);
+      });
+
+      return setTimeout(next, 200);
     }
 
     // try again in a bit
