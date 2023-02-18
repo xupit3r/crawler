454d7e01c15f6a1afdac955a8212d2cb9a42c079
Joe D'Alessandro <joe@thejoeshow.net>
2023-02-18 03:44:34 -0500

    some category generation stuff (using OpenAI) that i forgot to checkin

--- src/lib/learn.ts
+++ src/lib/learn.ts
@@ -507,13 +507,30 @@ export const classifyImages = async () => {
 }
 
 export const categorizeText = async () => {
-  const text = `
-    Black-on-black ware is a 20th- and 21st-century pottery tradition developed by the Puebloan Native American ceramic artists in Northern New Mexico. Traditional reduction-fired blackware has been made for centuries by pueblo artists. Black-on-black ware of the past century is produced with a smooth surface, with the designs applied through selective burnishing or the application of refractory slip. Another style involves carving or incising designs and selectively polishing the raised areas. For generations several families from Kha'po Owingeh and P'ohwhóge Owingeh pueblos have been making black-on-black ware with the techniques passed down from matriarch potters. Artists from other pueblos have also produced black-on-black ware. Several contemporary artists have created works honoring the pottery of their ancestors.
-  `;
+  await storage.connect();
 
-  const categories = await categoriesFromText(text);
+  const db = storage.db('crawler');
+  const text = db.collection('text');
 
-  console.log(categories);
+  const cursor = await text.find();
+
+  while (await cursor.hasNext()) {
+    const doc = await cursor.next();
+
+    if (doc) {
+      const categories = await categoriesFromText(doc.text.join(' '));
+
+      console.log(`categories for ${doc._id} -- ${categories.join(',')}`);
+
+      await text.updateOne({
+        _id: doc._id
+      }, {
+        $set: {
+          categories: categories
+        }
+      });
+    }
+  }
 
   exit();
 }
